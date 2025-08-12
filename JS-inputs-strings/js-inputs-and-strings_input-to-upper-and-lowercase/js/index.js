25383eebc3c1fd397acabf3ec656a9de0d34cc8b
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-08-12 11:00:18 +0200

    js-inputs-st…se-lowercase

--- JS-inputs-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ JS-inputs-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -6,3 +6,8 @@ const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
 });
+
+const button2 = document.querySelector('[data-js="button-uppercase2"]');
+button2.addEventListener("click", () => {
+  input.value = input.value.toLowerCase();
+});
