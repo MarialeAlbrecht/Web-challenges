a28f65371cc690d37c4e66b32d9549a878020357
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-08-27 16:15:11 +0200

    mock-interview/simple

--- Mock-Interview/simple-algorithms/index.js
+++ Mock-Interview/simple-algorithms/index.js
@@ -1,26 +1,65 @@
 // Find the maximum
 function maxOfTwoNumbers(num1, num2) {
-  // TODO:  
+  // TODO:
+  // 1. Identify variables. num1 and num2
+  // 2. Compare variables. If/else
+  // 3. Console.log the highest variable. Return
+  // 4. Call the function and add new values to the function.
+  if (num1 < num2) {
+    console.log(num2);
+  } else {
+    console.log(num1);
+  }
 }
 
+maxOfTwoNumbers(10, 7);
 
 // Find the longest word
 
-const words = ["Jaws", "Up", "Alien", "Gravity", "Inception", "Psycho",];
+const words = ["Jaws", "Up", "Alien", "Gravity", "Inception", "Psycho"];
 
 function findLongestWord(words) {
-  // TODO:  
+  // TODO:
+  // 1. Create a variable i, that changes and is replaced with the word content. Let i = 0
+  // 2. Create a variable longest that will store the new word if it is longer than the previous one. let longest = ""
+  // 2. Go through the array and compare each word lenght with the next one. Loop word[]
+  // 3. Create a the words and keep the one that has the longest length. If with i and .lenght
+  // 4. Console log the longest word. Return
+  // 5. Call the function.
+
+  let longest = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+  return longest;
 }
+
+console.log(findLongestWord(words));
 
 // Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers(numbers) {
-  // TODO:  
+  // TODO:
+  //1. Go/Loop throught the array. For with a let i =0
+  //2. Create a total variable with the value 0.
+  //2. Add the value of the current number to the previous one. .value and +
+  //3. Return the total value.
+  //4. Call the function
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
 }
 
-// Calculate the average length of the words 
+console.log(sumNumbers(numbers));
+
+// Calculate the average length of the words
 
 const words2 = [
   "eclipse",
@@ -32,59 +71,125 @@ const words2 = [
   "zenith",
   "ephemeral",
   "vortex",
-  "mystique"
+  "mystique",
 ];
 
 function averageWordLength(words) {
   // TODO:
+  //1. Create a loop to go to each of the strings.
+  //2. console.log each of the length of the words.
+  //3. Sum the length of all words and divided it by the number of words.
+  //4. Save the new value.
+  //5. Return the value
+  //6. Call the function
+
+  let totallength = 0;
+  for (let i = 0; i < words.length; i++) {
+    totallength += words[i].length;
+  }
+  return totallength / words.length;
 }
+
+console.log(averageWordLength(words2));
 
 // Unique arrays - return an array without duplicates
 
 const words3 = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
+  "crab",
+  "poison",
+  "contagious",
+  "simple",
+  "bring",
+  "sharp",
+  "playground",
+  "poison",
+  "communion",
+  "simple",
+  "bring",
 ];
 
 function uniquifyArray(words) {
   // TODO:
+  // 1. Create a forEach methods in a looü that will evaluate each of the elements in the array.
+  //2. Create and empty array that will creat the unique elements.
+  //3. Revise the current words with the method include and compare it with the next word.
+  //4. If the words is not the same as the previous one, add it to the new array.
+  //5. Return the new array.
+  //6. call the function.
+
+  let newarray = [];
+
+  words.forEach((word) => {
+    if (!newarray.includes(word)) {
+      newarray.push(word);
+    }
+  });
+  return newarray;
 }
 
+console.log(uniquifyArray(words3));
+
 // Find elements
-const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+const wordsFind = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
 function doesWordExist(haystack, needle) {
   // TODO:
+
+  //1. Create a loop and include the .find method.
+  //2. Revise if the word matches.
+  //3. Return a boolean.
+  //4. call the fucntion.
+
+  return haystack.includes(needle);
 }
+
+console.log(doesWordExist(wordsFind, "needle"));
 
 // Count repetition
 
 const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
+  "machine",
+  "matter",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "matter",
+  "truth",
+  "disobedience",
+  "matter",
 ];
 
 function howManyTimes(haystack, needle) {
   // TODO:
+  //1. Create a loop that for each repetition, it adds 1 to the count.
+  //2. Create a variable that will hold the counts.
+  //3. Compare it with the searched word.
+  //4. Return counts.
+  //5. Call function.
+
+  let count = 0;
+
+  haystack.forEach((word) => {
+    if (word === needle) {
+      count += 1;
+    }
+  });
+
+  return count;
 }
+
+console.log(howManyTimes(wordsCount, "matter"));
 
 // Bonus: A generic sum function
 // for strings use the length of the string, for booleans use 1 and 0
@@ -98,43 +203,136 @@ const mixedArray = [
   1995,
   23,
   true,
-  false
+  false,
 ];
 
 function sum(array) {
   // TODO:
+  //1. Create a variable to store the total.
+  //2. Go trought each element in the array.
+  //3. Read each element. if its a number add it, if is a string use the length, if is a boolean use 1 for true and 0 for false.
+  //4. Add the value to the total.
+  //5. Return the total.
+  //6. Call the function.
+
+  let total = 0;
+
+  array.forEach((item) => {
+    if (typeof item === "number") {
+      total += item;
+    } else if (typeof item === "string") {
+      total += item.length;
+    } else if (typeof item === "boolean") {
+      total += item ? 1 : 0;
+    }
+  });
+
+  return total;
 }
+
+console.log(sum(mixedArray));
 
 // Bonus: Write a function that calculates the greatest product of four
 // numbers that is either horizontally or vertically in the array
 
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+  [
+    49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62,
+    0,
+  ],
+  [
+    81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36,
+    65,
+  ],
   [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+  [
+    22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13,
+    80,
+  ],
+  [
+    24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12,
+    50,
+  ],
+  [
+    32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64,
+    70,
+  ],
+  [
+    67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94,
+    21,
+  ],
+  [
+    24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63,
+    72,
+  ],
   [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
   [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+  [
+    16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85,
+    57,
+  ],
   [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+  [
+    19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55,
+    40,
+  ],
   [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+  [
+    88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53,
+    69,
+  ],
+  [
+    4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76,
+    36,
+  ],
+  [
+    20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36,
+    16,
+  ],
+  [
+    20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5,
+    54,
+  ],
+  [
+    1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67,
+    48,
+  ],
 ];
 
 function greatestProduct(matrix) {
   // TODO:
-}
+  //1. Create a variable that will store the product.
+  //2. Loop for each of the objects.
+  //3. Compare the horizontal and vertical products.
+  //4. Return the highest product.
+  //5. Call the function
 
+  let maxProduct = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length - 4; j++) {
+      const product =
+        matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
+      if (product > maxProduct) {
+        maxProduct = product;
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length - 4; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const product =
+        matrix[i][j] * matrix[i + 1][j] * matrix[i + 2][j] * matrix[i + 3][j];
+      if (product > maxProduct) {
+        maxProduct = product;
+      }
+    }
+  }
+
+  return maxProduct;
+}
+console.log(greatestProduct(matrix));
 
 module.exports = {
   maxOfTwoNumbers,
@@ -145,5 +343,5 @@ module.exports = {
   doesWordExist,
   howManyTimes,
   sum,
-  greatestProduct 
-}
+  greatestProduct,
+};
