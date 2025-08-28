e03eab019ec31c7cfa8d584caff171353c5674b1
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-08-28 15:13:11 +0200

    deleted code line

--- React-Arrays/react-with-arrays_fruits/src/App.jsx
+++ React-Arrays/react-with-arrays_fruits/src/App.jsx
@@ -33,7 +33,6 @@ export default function App() {
     <div className="app">
       {fruits.map((fruit) => (
         <li key={fruit.id}>
-          {" "}
           <Card name={fruit.name} color={fruit.color} />
         </li>
       ))}
